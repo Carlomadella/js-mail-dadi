03170c43f63cbe3745b37dc321a512d81d0d6e8f
Carlomadella <madella871@gmail.com>
2025-03-20 15:16:14 +0100

    creato ciclo for

--- js/script.js
+++ js/script.js
@@ -6,15 +6,24 @@
 
 // 1. Creare una array con lista di email di invitati ad una festa.
 // 2. Chiedere all’utente la sua email
-// 3. controllare che sia nella lista di chi può accedere.
+// 3. creare ciclo for per controllare che sia nella lista di chi può accedere.
+//  3a. 
 // 4. Stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for. 
 // Nota: Non è necessario provvedere alla validazione delle email
 
+// array delle mail
 const mailList = ["BrunoBarbieri@libero.it", "Travis_Scott@houston.com", "RiccardoRossi@mozzilla.net", "AntonioBianchi@repair.it", "Rick@Sanchez.morty", "Boolean@classe143.it"];
-
+// variabile che contiene l'email 
 const userEmail = prompt("Inserisci la tua email");
 
-console.log(userEmail);
+// console.log(userEmail);
 
-const checkList = false;
+let checkList = false;
+
+for (let i = 0; i < mailList.length; i++) {
+    if (mailList[i] == userEmail) {
+        checkList = true;
+    }
+}
+
 
